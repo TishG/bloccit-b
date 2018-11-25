99fa562590353556b20ff099a9cacf874164198d
Tish <latisha.griffiths@gmail.com>
2018-11-25 11:01:30 -0600

    Completed Checkpoint: voting

---
+++ src/db/models/vote.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Vote = sequelize.define('Vote', {
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //Validate that value is not null and set to either -1 or 1
+      validate: {
+        isIn: [[-1, 1]]
+      }
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {});
+  Vote.associate = function(models) {
+    Vote.belongsTo(models.Post, {
+      foreignKey: "postId",
+      onDelete: "CASCADE"
+    });
+
+    Vote.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE"
+    });
+  };
+  return Vote;
+};

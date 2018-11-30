3c8bcc5fa7277525fcc5027fc0e2abad1a07d792
Tish <latisha.griffiths@gmail.com>
2018-11-30 14:55:31 -0600

    Redid voting checkpoint, all tests pass

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -23,6 +23,10 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "comments"
     });
+    Post.hasMany(models.Vote, {
+      foreignKey: "postId",
+      as: "votes"
+    });
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
       onDelete: "CASCADE"
@@ -32,5 +36,11 @@ module.exports = (sequelize, DataTypes) => {
       onDelete: "CASCADE"
     });
   };
+  Post.prototype.getPoints = function(){
+        if(this.votes.length === 0) return 0
+        return this.votes
+          .map((v) => { return v.value })
+          .reduce((prev, next) => { return prev + next });
+      };
   return Post;
 };

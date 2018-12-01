eb00af8dd7fc243a275b6dc0a4af0ce7aefd317b
Tish <latisha.griffiths@gmail.com>
2018-12-01 08:40:11 -0600

    Completed Assignment: Public Profiles

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,6 +1,7 @@
 const User = require("./models").User;
 const Post = require("./models").Post;
 const Comment = require("./models").Comment;
+const Favorite = require("./models").Favorite;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
@@ -32,14 +33,21 @@ module.exports = {
              Comment.scope({method: ["lastFiveFor", id]}).all()
              .then((comments) => {
                result["comments"] = comments;
+               Favorite.scope({method: ["favoritePosts", id]}).all()
+               .then((favorites) => {
+                 result["favorites"] = favorites;
                callback(null, result);
              })
              .catch((err) => {
                callback(err);
              })
            })
-         }
+           .catch((err) => {
+            callback(err);
+         })
        })
      }
+    })
+  }
 
 }

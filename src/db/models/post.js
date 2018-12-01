26800472e15116bb8766beb854a15be2004f8410
Tish <latisha.griffiths@gmail.com>
2018-11-30 23:50:00 -0600

    Completed Assignment: Favoriting

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -37,6 +37,13 @@ module.exports = (sequelize, DataTypes) => {
         postId: post.id
       });
     });
+    Post.afterCreate((post, callback) => {
+      return models.Vote.create({
+        value: 1,
+        userId: post.userId,
+        postId: post.id
+      });
+    });
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
       onDelete: "CASCADE"
